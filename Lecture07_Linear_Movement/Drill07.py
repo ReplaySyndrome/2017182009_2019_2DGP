aa674807fe8b2857ec82c982064d51459f399934
kisum1024 <43131669+kisum1024@users.noreply.github.com>
2019-10-09 14:49:12 +0900

    도중에 경로 변경 가능

--- Lecture07_Linear_Movement/Drill07.py
+++ Lecture07_Linear_Movement/Drill07.py
@@ -4,29 +4,28 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
+    global x,y
+    global to_x, to_y
+    global move
+    global movedirection
+    global i
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         if event.type == SDL_MOUSEBUTTONDOWN:
-            to_x, to_y = event.x, KPU_HEIGHT -1 - event.y
+            to_x, to_y = event.x, KPU_HEIGHT - 1 - event.y
+            if to_x > x:
+                movedirection = 1
+            else:
+                movedirection = 0
+            i=0
+            move = True
 
 
-def go_to_destination(p1,p2):
-    global frame
-    global x, y
-    for i in range(0, 100 + 1, 2):
-        clear_canvas()
-        kpu_ground.draw(1280//2,1024//2)
-        t = i / 100
-        x = (1 - t) * p1[0] + t * p2[0]
-        y = (1 - t) * p1[1] + t * p2[1]
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-        update_canvas()
-        frame = (frame + 1) % 8
-        delay(0.05)
 
-    Move = False
+
+
 
 
 
@@ -34,6 +33,7 @@ def go_to_destination(p1,p2):
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+hide
 
 
 running = True
@@ -43,14 +43,33 @@ frame = 0
 to_x = 800
 to_y = 600
 animation_state = 3
-
-Move = True
+movedirection = 1
+i = 1000
+move = False
 
 while running:
-    if Move == True:
-        go_to_destination((x, y), (to_x, to_y))
+    if move:
+        while i < 1000:
+            clear_canvas()
+            kpu_ground.draw(1280 // 2, 1024 // 2)
+            t = i / 1000
+            x = (1 - t) * x + t * to_x
+            y = (1 - t) * y + t * to_y
+            character.clip_draw(frame * 100, 100 * movedirection, 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            handle_events()
+            i += 1
+            delay(0.05)
+        move = False
     else:
+        clear_canvas()
+        kpu_ground.draw(1280 // 2, 1024 // 2)
         character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+    handle_events()
 
 
 
