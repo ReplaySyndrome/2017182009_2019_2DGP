373372ed4a3054e8ed9f16e3d732a24a9e6749f3
kisum1024 <43131669+kisum1024@users.noreply.github.com>
2019-10-09 14:30:45 +0900

    목표지점에가면 제자리에서 달림

--- Lecture07_Linear_Movement/Drill07.py
+++ Lecture07_Linear_Movement/Drill07.py
@@ -9,7 +9,26 @@ def handle_events():
         if event.type == SDL_QUIT:
             running = False
         if event.type == SDL_MOUSEBUTTONDOWN:
-            pass
+            to_x, to_y = event.x, KPU_HEIGHT -1 - event.y
+
+
+def go_to_destination(p1,p2):
+    global frame
+    global x, y
+    for i in range(0, 100 + 1, 2):
+        clear_canvas()
+        kpu_ground.draw(1280//2,1024//2)
+        t = i / 100
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+
+    Move = False
+
+
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -21,18 +40,17 @@ running = True
 x = 800 // 2
 y = 90
 frame = 0
-to_x = 800//2
-to_y = 90
+to_x = 800
+to_y = 600
 animation_state = 3
 
-while running:
-    clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * animation_state, 100, 100, x, y)
-    update_canvas()
+Move = True
 
-    handle_events()
-    frame = (frame + 1) % 8
+while running:
+    if Move == True:
+        go_to_destination((x, y), (to_x, to_y))
+    else:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
 
 
 
