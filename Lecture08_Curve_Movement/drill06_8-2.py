d13acb582c4e792c86cc2762cae7ea86ab186e30
kisum1024 <43131669+kisum1024@users.noreply.github.com>
2019-10-11 11:42:20 +0900

    character move

--- Lecture08_Curve_Movement/drill06_8-2.py
+++ Lecture08_Curve_Movement/drill06_8-2.py
@@ -4,36 +4,33 @@ import random
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
-#total commit time is 7
-
-
-
-
-
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
-
-
-
 
 running = True
 x = 800 // 2
 y = 90
 frame = 0
 size = 10
-points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+points = [(random.randint(0, 1280), random.randint(0, 1024)) for i in range(size)]
 
-print(points)
+def character_move(p1,p2,p3,p4):
+    global frame
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        clear_canvas()
+        kpu_ground.draw(1280 // 2, 1024 // 2)
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+
 
 while running:
-    clear_canvas()
-    kpu_ground.draw(1280 // 2, 1024 // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
-    delay(0.05)
-
+    character_move(points[-1],points[-0],points[1],points[2])
 
 
 
