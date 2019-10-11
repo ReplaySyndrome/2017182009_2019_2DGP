abb34f3548351ce4a01ffbe34beb6bc8b57bad08
kisum1024 <43131669+kisum1024@users.noreply.github.com>
2019-10-11 11:21:38 +0900

    drill06 drawline

--- Lecture08_Curve_Movement/Drill06.py
+++ Lecture08_Curve_Movement/Drill06.py
@@ -95,6 +95,32 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
+def first_curve(p1,p2,p3):
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+        draw_point((x, y))
+    draw_point(p2)
+
+def second_curve(p1,p2,p3,p4):
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        draw_point((x, y))
+    draw_point(p3)
+
+
+def draw_curve_4_points_smooth(p1,p2,p3,p4):
+    n = 0
+    size = 4
+    while True:
+        second_curve(p1,p2,p3,p4)
+        second_curve(p2,p3,p4,p1)
+        second_curve(p3,p4,p1,p2)
+        second_curve(p4,p1,p2,p3)
+
 
 
 
@@ -103,7 +129,9 @@ prepare_turtle_canvas()
 
 
 
-draw_curve_4_points((-350, -100), (-50, 200), (150, -100), (350, 300))
+draw_curve_4_points_smooth((-300, 200), (400, 350), (300, -300), (-200,-200))
+
+
 
 
 turtle.done()
